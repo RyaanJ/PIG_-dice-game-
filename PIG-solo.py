311c245a10d720e4002dd93eb1d92f1cc7f4ef89
RyaanJ <ryaannaumanjaffar@gmail.com>
2024-06-17 02:59:45 -0400

    added lots of print lines in solo.py and updated readme

--- PIG-solo.py
+++ PIG-solo.py
@@ -1,5 +1,6 @@
 import random
-
+print("Hello there! Welcome to PIG, a simple game of luck! \n")
+print("Score ceiling is 50! Try and get the highest score possible without rolling a 1. \n")
 roll = input("Do you want to Roll? (yes/no): ").lower()
 
 while roll != "yes" and roll != "no":
@@ -22,4 +23,5 @@ while roll == "yes" and score < 50:
     roll = input("Do you want to Roll? (yes/no): ").lower()
     if roll == "no":
         print("Thanks For Playing!")
+        print(f"You ended with a total score of {score}. Congrats on not rolling a 1!")
         break
